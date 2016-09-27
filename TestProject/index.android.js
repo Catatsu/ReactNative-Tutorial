b0eed383e90c25e50a76c91eb8ba094c79d678ac
matumotto <matsumoto_takuya@hasigo.co.jp>
2016-09-27 23:52:14 +0900

    文言変更

--- TestProject/index.android.js
+++ TestProject/index.android.js
@@ -17,7 +17,7 @@ class TestProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hello React!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
